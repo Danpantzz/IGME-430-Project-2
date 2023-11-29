a44356bd3dd4b6cf17e337376fb7280be65106a7
Danpantzz <danpantz@gmail.com>
2023-11-28 21:15:20 -0500

    created canvas element

--- client/maker.jsx
+++ client/maker.jsx
@@ -2,78 +2,19 @@ const helper = require('./helper.js');
 const React = require('react');
 const ReactDOM = require('react-dom');
 
-const handleDomo = (e) => {
-    e.preventDefault();
-    helper.hideError();
-
-    const name = e.target.querySelector('#domoName').value;
-    const age = e.target.querySelector('#domoAge').value;
-    const level = e.target.querySelector('#domoLevel').value;
-
-    if (!name || !age || !level) {
-        helper.handleError('All fields are required!');
-        return false;
-    }
-
-    helper.sendPost(e.target.action, { name, age, level }, loadDomosFromServer);
-
-    return false;
-}
-
-const DomoForm = (props) => {
+const DrawCanvas = (props) => {
     return (
-        <form id='domoForm'
-            onSubmit={handleDomo}
-            name='domoForm'
-            action='/maker'
-            method='POST'
-            className='domoForm'
-        >
-            <label htmlFor='name'>Name: </label>
-            <input id='domoName' type='text' name='name' placeholder='Domo Name' />
-            <label htmlFor='age'>Age: </label>
-            <input id='domoAge' type='number' min='0' name='age' />
-            <label htmlFor='level'>Level: </label>
-            <input id='domoLevel' type='number' min='0' name='level' />
-            <input className='makeDomoSubmit' type='submit' value='Make Domo' />
-        </form>
+        <canvas id="myCanvas" width={window.innerWidth * .5} height={window.innerHeight * .8}></canvas>
     );
 }
 
-const DomoList = (props) => {
-    if (props.domos.length === 0) {
-        return (
-            <div className='domoList'>
-                <h3 className='emptyDomo'>No Domos Yet!</h3>
-            </div>
-        );
-    }
+const drawPlayer = () => {
+    const canvas = document.getElementById('myCanvas');
+    const ctx = canvas.getContext('2d');
+    ctx.beginPath();
+    ctx.arc(canvas.width / 2, canvas.height / 2, 20, 0, 2 * Math.PI);
+    ctx.stroke();
 
-    const domoNodes = props.domos.map(domo => {
-        return (
-            <div key={domo._id} className='domo'>
-                <img src='/assets/img/domoface.jpeg' alt='domo face' className='domoFace' />
-                <h3 className='domoName'> Name: {domo.name} </h3>
-                <h3 className='domoAge'> Age: {domo.age} </h3>
-                <h3 className='domoLevel'> Level: {domo.level} </h3>
-            </div>
-        );
-    });
-
-    return (
-        <div className='domoList'>
-            {domoNodes}
-        </div>
-    );
-}
-
-const loadDomosFromServer = async () => {
-    const response = await fetch('/getDomos');
-    const data = await response.json();
-    ReactDOM.render(
-        <DomoList domos={data.domos} />,
-        document.getElementById('domos')
-    );
 }
 
 const handleChangePassword = (e) => {
@@ -128,6 +69,12 @@ const ChangePasswordWindow = (props) => {
     );
 }
 
+const changeCanvasSize = () => {
+    let canvas = document.getElementById('myCanvas');
+    canvas.width = window.innerWidth * .5;
+    canvas.height = window.innerHeight * .8;
+}
+
 const init = () => {
     const changePasswordButton = document.getElementById('changePassword');
 
@@ -141,16 +88,13 @@ const init = () => {
     });
 
     ReactDOM.render(
-        <DomoForm />,
-        document.getElementById('makeDomo')
+        <DrawCanvas />,
+        document.getElementById('canvasContainer')
     );
 
-    ReactDOM.render(
-        <DomoList domos={[]} />,
-        document.getElementById('domos')
-    );
+    drawPlayer();
 
-    loadDomosFromServer();
 }
 
 window.onload = init;
+window.onresize = changeCanvasSize;
